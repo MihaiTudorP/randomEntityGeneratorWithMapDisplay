7627335926a0c88272e5fa60c89f663be62a9146
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-02-13 23:11:21 +0200

    Added markers to the map

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,4 +1,6 @@
-export class CustomMap{
+import {Mappable} from './interfaces/Mappable';
+
+export class CustomMap {
     private mapObject: google.maps.Map;
 
     constructor(containerId: string) {
@@ -6,5 +8,9 @@ export class CustomMap{
             zoom: 1,
             center: {lat: 0, lng: 0}
         });
+    }
+
+    addMarker(mappable: Mappable): void {
+        const marker = new google.maps.Marker({map: this.mapObject, position: mappable.location});
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-// import {User} from './User';
-// import {Company} from './Company';
+import {User} from './User';
+import {Company} from './Company';
 
 import {CustomMap} from './CustomMap';
 
 const customMap = new CustomMap('map');
+const user = new User();
+customMap.addMarker(user);
+customMap.addMarker(new Company());
